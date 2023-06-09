dd99046d21b89ff121baad9d36ba504f3596f96c
Jordan <jordan@murasaki>
2023-06-09 19:15:49 -0400

    some dark theme adjustments

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="bg-white fixed bottom-0">
-      <a target="_blank" href="https://icons8.com/icon/38555/cydia">Cydia</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+    <footer className="bg-white fixed bottom-0 dark:bg-black">
+      <a target="_blank" href="https://icons8.com/icon/38555/cydia" className="dark:text-white" >Cydia</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
     </footer>
-    );
+  );
 }
