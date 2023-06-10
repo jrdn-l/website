b78861e4673b002e33f10833db725df4ede4aff1
Jordan <jordan@murasaki>
2023-06-09 20:17:52 -0400

    fixes layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import Layout from "@/components/layout"
 import Image from "next/image"
 
 export default function Home() {
   return (
-    <Layout>
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <Image width={240} height={240} src={"/hello-wave.gif"} alt="Github" />
-      </main>
-    </Layout>
+    <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+      <Image width={240} height={240} src={"/hello-wave.gif"} alt="Github" />
+      <p className="h-96 dark:text-white">Jello</p>
+      <p className="h-96 dark:text-white"> Hello</p>
+      <p className="h-96 dark:text-white"> Fellow</p>
+    </main>
   )
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,10 +8,15 @@ export default function Header() {
         <button className="text-sm font-semibold leading-6 text-gray-900 dark:text-white">Projects</button>
 
         <a rel="noopener noreferrer" target="_blank" href="https://github.com/jrdn-l" className="-m-1.5 px-3">
-          <Image className="h-8 w-auto" width={240} height={240} src={"/github-mark.svg"} alt="Github" />
+          <picture>
+            <source srcSet={"/github-mark-white.svg"} media="(prefers-color-scheme: dark)" />
+            <Image className="h-8 w-auto" width={240} height={240} src={"/github-mark.svg"} alt="Github" />
+          </picture>
         </a>
         <a rel="noopener noreferrer" target="_blank" href="https://jrdn-l.github.io/myrepo/" className="-m-1.5 px-3">
-          <Image className="h-8 w-auto" width={240} height={240} src="/icons8-cydia-240.svg" alt="Cydia" />
+          <picture>
+            <Image className="h-8 w-auto" width={240} height={240} src="/icons8-cydia-240.svg" alt="Cydia" />
+          </picture>
         </a>
       </nav>
     </header>
