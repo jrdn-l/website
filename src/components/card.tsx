cb33a6618023cc2ea41a9e5d2cd26d5e0483ce43
Jordan <jordan@murasaki>
2023-06-10 14:40:47 -0400

    adds new component, removes unneeded dark classes

---
+++ src/components/card.tsx
@@ -0,0 +1,9 @@
+export default function Card({ setShowModal, title, description }: { setShowModal: (showModal: boolean) => void, title: string, description: string }) {
+  return (
+    <div className="drop-shadow-lg rounded-lg p-4 m-4 bg-slate-50 dark:bg-gray-800 hover:opacity-90 hover:scale-105 max-w-md max-h-60 overflow-y-scroll"
+      onClick={() => { setShowModal(true) }}>
+      <h2 className=" font-bold text-xl">{title}</h2>
+      <p className="text-sm">{description}</p>
+    </div>
+  )
+}
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,8 +5,8 @@ export default function Header() {
   return (
     <header>
       <nav className="flex flex-center items-center justify-evenly p-6">
-        <Link className="text-sm font-semibold leading-6 text-gray-900 dark:text-white" href="/">Home</Link>
-        <Link className="text-sm font-semibold leading-6 text-gray-900 dark:text-white" href="#projects" scroll={false}>Projects</Link>
+        <Link className="text-sm font-semibold leading-6" href="/">Home</Link>
+        <Link className="text-sm font-semibold leading-6" href="#projects" scroll={false}>Projects</Link>
 
         <a rel="noopener noreferrer" target="_blank" href="https://github.com/jrdn-l" className="-m-1.5 px-3">
           <picture>
